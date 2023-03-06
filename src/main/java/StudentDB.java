2f39352a9c27650369fe770a4671dbaa75ac600a
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2023-03-06 18:30:26 +0100

    Add Bonus Tasks

--- src/main/java/StudentDB.java
+++ src/main/java/StudentDB.java
@@ -16,6 +16,27 @@ public class StudentDB {
         return students[random];
     }
 
+    public void addStudent(Student studentToAdd){
+        Student[] newStudentArray = new Student[students.length + 1];
+        for (int i = 0; i < students.length; i++) {
+            newStudentArray[i] = students[i];
+        }
+        newStudentArray[newStudentArray.length - 1] = studentToAdd;
+        students = newStudentArray;
+    }
+
+    public void removeStudent(Student studentToRemove){
+        Student[] newStudentArray = new Student[students.length - 1];
+        int index = 0;
+        for (int i = 0; i < students.length; i++) {
+            if (students[i] != studentToRemove){
+                newStudentArray[index] = students[i];
+                index++;
+            }
+        }
+        students = newStudentArray;
+    }
+
     @Override
     public String toString() {
         return "StudentDB{" +
--- src/test/java/StudentDBTest.java
+++ src/test/java/StudentDBTest.java
@@ -20,4 +20,35 @@ class StudentDBTest {
         Student[] expected = {new Student("123", "Max")};
         assertArrayEquals(actual, expected);
     }
+
+    @Test
+    void addStudent() {
+        //GIVEN
+        Student max = new Student("123", "Max");
+        Student[] testStudents = new Student[0];
+        StudentDB studentDb = new StudentDB(testStudents);
+
+        //WHEN
+        studentDb.addStudent(max);
+
+        //THEN
+        Student[] expected = new Student[]{new Student("123", "Max")};
+        assertArrayEquals(expected, studentDb.getAllStudents());
+    }
+
+    @Test
+    void removeStudent() {
+        //GIVEN
+        Student max = new Student("123", "Max");
+        Student[] testStudents = new Student[1];
+        testStudents[0] = max;
+        StudentDB studentDb = new StudentDB(testStudents);
+
+        //WHEN
+        studentDb.removeStudent(max);
+
+        //THEN
+        Student[] expected = new Student[0];
+        assertArrayEquals(expected, studentDb.getAllStudents());
+    }
 }
